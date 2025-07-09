1691f60c67b9f42e2bce278b2742b6e3bb2a1dd4
maryam <maryamlutbaishi213@hotmail.com>
2025-07-09 11:41:06 +0300

    add duck class

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,20 @@
 function init(){
     //all code goes here
     const gridElem=document.querySelector('.grid')
+    const cells=[]
+    const gridWidth=10
+    const numberOfCells= gridWidth*gridWidth
     function createGrid(){
         // for every cell that we require creat a div
         //append this cell to our gris
         for(let i=0;i<100;i++){
             const cell = document.createElement('div')
+            cell.classList.add('duck')
             cell.textContent=i
-            
+            cells.push(cell)
             gridElem.appendChild(cell)
         }
+        console.log(cells)
     }
     createGrid()
 }
