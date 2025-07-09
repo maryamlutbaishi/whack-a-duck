8157ba54360f8b3cdb66d5e250485b58cda32162
maryam <maryamlutbaishi213@hotmail.com>
2025-07-09 13:57:00 +0300

    add handel click incremeant the score

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 function init(){
     //all code goes here
     const gridElem=document.querySelector('.grid')
-    
+    const scoreElem=document.querySelector('#Score-display')
     
     const cells=[]
     const gridWidth=10
     const numberOfCells= gridWidth*gridWidth
 
     let duckPosition= 20
+    let score= 0
 
     function addDuck(){
         cells[duckPosition].classList.add('duck')
@@ -21,9 +22,16 @@ function init(){
             removeDuck()
             duckPosition=Math.floor(Math.random()*numberOfCells)
             addDuck()
-        },1000)
+        },3000)
     }
-    
+    function handeleClick(event){
+        console.log('Handel Click has run!')
+        if(event.target.classList.contains('duck')){
+            score += 10
+            scoreElem.textContent=`Your Score is ${score}`
+            console.log(score)
+        }
+    }
     function createGrid(){
         // for every cell that we require creat a div
         //append this cell to our gris
@@ -31,6 +39,7 @@ function init(){
             const cell = document.createElement('div')
             // cell.classList.add('duck')
             cell.textContent=i
+            cell.addEventListener('click',handeleClick)
             cells.push(cell)
             gridElem.appendChild(cell)
         }
